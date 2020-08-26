ce7c4b1be8d2eea35c6187e11d6a98cbb2a977d8
Bryan Dollery <bryan.dollery@knowledgehut.co>
2020-08-26 09:45:26 +0000

    working with ingress

--- pro-graf.sh
+++ pro-graf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl create namespace monitor
 helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer
-
+kubectl apply -f ingress.yaml -n monitor
